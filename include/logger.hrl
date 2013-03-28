e9544cb8d3100c83e4d21444bc14d29a70fc9c3b
redink <cnredink@gmail.com>
2013-03-28 04:50:20 -0700

    modified include files

--- include/logger.hrl
+++ include/logger.hrl
@@ -1,11 +1,3 @@
--define(ASSERT_EQ(A, B), if 
-			     A =:= B -> ok;
-			     true -> error_logger:info_report({module,?MODULE},
-							      {line,?LINE},
-							      {self,self()},
-							      {message "Assert equal fail"})
-			 end).
-
 -define(LOG(Message), error_logger:info_report([{module, ?MODULE}, 
                               {line, ?LINE},
                               {self, self()}, 
