8970f70df0567de3725e6193bb2e91e4e9f8dcab
redink <cnredink@gmail.com>
2013-03-28 06:15:29 -0700

    simple README

---
+++ include/records.hrl
@@ -0,0 +1,8 @@
+-record(client, {
+          socket,
+          transport,
+          begin_module,
+          build_module,
+          parse_module,
+          keepalive_ref
+}).
--- src/tcpserver_proto_parse.erl
+++ src/tcpserver_proto_parse.erl
@@ -8,19 +8,12 @@
 %%%-------------------------------------------------------------------
 -module(tcpserver_proto_parse).
 
-
-
 -compile(export_all).
 -include("proto.hrl").
 -include("logger.hrl").
 
 parse(<< Size:32/big,Checksum:8,Command:16/big, Data/bits >>) ->
-    ?LOG(["Size: ",Size," Checksum: ",Checksum," Command: ",Command," Data: ",Data]),
     parse(Size, Command, Checksum, Data).
 
-parse(_Size,?CS_USER_LOGIN,_Checksum,Data)->
-    {login,?CS_USER_LOGIN,Data};
-
 parse(_Size,_Command,_Checksum,_Data)->
-    ?LOG("ignore proto_parse"),
     ignore.
